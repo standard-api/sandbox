342f66f78c43824f060ee9a575cb61c4a6a093c8
clickout <mick.voros@gmail.com>
2023-07-19 14:47:04 +0200

    Setup to use arango

--- src/main/java/ai/stapi/sandbox/ArangoSandboxApplication.java
+++ src/main/java/ai/stapi/sandbox/ArangoSandboxApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SandboxApplication {
+public class ArangoSandboxApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SandboxApplication.class, args);
+		SpringApplication.run(ArangoSandboxApplication.class, args);
 	}
 }
